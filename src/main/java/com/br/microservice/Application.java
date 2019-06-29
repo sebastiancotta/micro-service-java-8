d2ae7f5bb25ef8bc98abd4532e224b6dd1030795
sebastiancotta <sebastiancotta@gmail.com>
2019-06-29 08:52:30 -0300

    testando o gerador de senha

--- src/main/java/com/br/microservice/Application.java
+++ src/main/java/com/br/microservice/Application.java
@@ -1,5 +1,6 @@
 package com.br.microservice;
 
+import com.br.microservice.utils.SenhaUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+        System.out.println(SenhaUtils.gerarBCript("$2a$10$NCYZV.u2EI7KuimjvGdGT.GMAFeTCtqEq2tVM1VA2f3NqMTGYGXqa"));
+        System.out.println(SenhaUtils.validaSenha("$2a$10$NCYZV.u2EI7KuimjvGdGT.GMAFeTCtqEq2tVM1VA2f3NqMTGYGXqa", "$2a$10$k2jpuTrf8pa7ro17fjGgWewcTtHLqoTbiPBdQe4ngAwJtDk6id.P2"));
     }
 }
